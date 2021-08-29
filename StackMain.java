0409683fe6ad12767dedb6e00076c741c6e86358
asimbinahammed <asim242370@gmail.com>
2021-08-29 16:01:40 +0530

    Push commands of stack

---
+++ StackMain.java
@@ -0,0 +1,14 @@
+package bridgelabz;
+
+
+public class StackMain {
+	public static void main(String[] args) {
+		Stack stack = new Stack();
+		stack.push(70);
+		stack.push(30);
+		stack.push(56);
+		
+		stack.displayStackElements();
+
+	}
+}
