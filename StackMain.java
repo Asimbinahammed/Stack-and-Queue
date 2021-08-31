d47e559dc70cc4f12e7078b500cd4e59c5c5bd6e
asimbinahammed <asim242370@gmail.com>
2021-08-31 22:53:13 +0530

    Updated Linked list code implementing stack

---
+++ StackMain.java
@@ -0,0 +1,18 @@
+package com.bridgelabz;
+
+//main class
+public class StackMain {
+	public static void main(String[] args) {
+		// create Object of Implementing class
+		StackUsingLinkedlist obj = new StackUsingLinkedlist();
+		// insert Stack value
+		obj.push(70);
+		obj.push(30);
+		obj.push(56);
+
+		// print Stack elements
+		obj.display();
+
+		
+	}
+}
