f543cbe51bc2d4e41ceeecdf68da447d35942c70
asimbinahammed <asim242370@gmail.com>
2021-09-03 22:41:26 +0530

    Pushing into stack

---
+++ StackMain.java
@@ -0,0 +1,31 @@
+package stackandqueue;
+//main class
+public class StackMain {
+	public static void main(String[] args) {
+		// create Object of Implementing class
+		StackUsingLinkedlist obj = new StackUsingLinkedlist();
+		// insert Stack value
+		obj.push(70);
+		obj.push(30);
+		obj.push(56);
+		// print Stack elements
+		obj.display();
+		
+//		System.out.println("");
+//		obj.peek();
+//		obj.pop();
+//		System.out.println();
+//
+//		obj.display();
+//		obj.peek();
+//		obj.pop();
+//		System.out.println();
+//
+//		obj.display();
+//		obj.peek();
+//		obj.pop();
+//		
+//		obj.display();
+		
+	}
+}
---
+++ StackUsingLinkedlist.java
@@ -0,0 +1,69 @@
+package stackandqueue;
+
+
+import static java.lang.System.exit;
+
+class StackUsingLinkedlist {
+	private class Node {
+
+		int data;
+		Node link;
+	}
+
+	Node top;
+
+	StackUsingLinkedlist() {
+		this.top = null;
+	}
+
+	@SuppressWarnings("unused")
+	public void push(int x) {
+		Node temp = new Node();
+
+		if (temp == null) {
+			System.out.print("\nHeap Overflow");
+			return;
+		}
+		temp.data = x;
+		temp.link = top;
+		top = temp;
+	}
+
+	// check empty function
+	public boolean isEmpty() {
+		return top == null;
+	}
+
+	// return top element in a stack
+	public void peek() {
+		if (!isEmpty()) {
+			System.out.println("top element in stack is "+top.data);
+		} else {
+			System.out.println("Stack is empty");
+		}
+	}
+
+	// pop top element from the stack
+	public void pop() {
+		if (top == null) {
+			System.out.print("\nStack Underflow");
+			return;
+		}
+		System.out.println("poped element is" + top.data);
+		top = (top).link;
+	}
+
+	public void display() {
+		if (top == null) {
+			System.out.printf("\nStack Underflow");
+			exit(1);
+		} else {
+			Node temp = top;
+			while (temp != null) {
+				System.out.println( temp.data);
+				temp = temp.link;
+			}
+		}
+	}
+}
+
